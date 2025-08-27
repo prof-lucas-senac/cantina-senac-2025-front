e59d1bd2210ec5e2e302b1438bda046f719a8378
jvictormelo <jvmelo0x@gmail.com>
2025-08-26 21:55:16 -0300

    Att html login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login', //ingetar o do componente no app.module.ts
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
