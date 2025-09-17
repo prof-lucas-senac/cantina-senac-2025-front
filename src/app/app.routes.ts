59fcb5587276d3b20e93a559d31fdf76af9458a1
Isa Padilha <isap4619@gmail.com>
2025-09-16 21:38:21 -0300

    Implementado Rounting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+
+    {
+        path: "feedback",
+        component: FeedbackComponent
+    },
+
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+];
