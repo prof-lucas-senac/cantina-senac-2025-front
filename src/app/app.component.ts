e2ac8dcccab5679241dbd12bd4bd85e7d82b8f32
Valmir <94.valmirjr@gmail.com>
2025-09-16 21:37:29 -0300

    roteamento entre login e feedbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { FeedbacksComponent } from './components/feedbacks/feedbacks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent, FeedbacksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
