da9da20c1d0a9da3efc4e75ebc2493d03dff3f40
Junior Godoi <juniorgodoi1987@gmail.com>
2025-09-16 21:37:37 -0300

    roteamento de páginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import {FeedbacksComponent} from './components/feedbacks/feedbacks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, LoginComponent, FeedbacksComponent],
+  templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
