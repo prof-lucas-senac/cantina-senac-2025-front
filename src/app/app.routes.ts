31ce5cd390879a527f6f0b15e51ebcc9de6b830f
AdrieMota <adrimota@uniplaclages.edu.br>
2025-09-16 21:36:34 -0300

    Roteamento das paginas feedback e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+   
+    {
+        path: "feedbacks",
+        component: FeedbackComponent
+    },
+
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+];
+
