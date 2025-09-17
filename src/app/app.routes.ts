d1542589eedb5b817658ed7335d0b22d08dc063c
jvictormelo <jvmelo0x@gmail.com>
2025-09-16 21:56:13 -0300

    Att com redirectTo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FeedbacksComponent } from './components/feedbacks/feedbacks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+        
+    },
+    {
+        path:"feedbacks",
+        component:FeedbacksComponent
+    },
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+    
+];
