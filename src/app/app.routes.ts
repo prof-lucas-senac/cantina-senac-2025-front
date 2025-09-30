c92524508e3ea456e57145735e6a9cbc6905a054
Marco12br <marcoantoniobueno12@gmail.com>
2025-09-30 19:17:53 -0300

    checkpoint antes do pull na master

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FeedbacksComponent } from './components/feedbacks/feedbacks.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: "feedbacks",
+        component: FeedbacksComponent
+    },
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+];
