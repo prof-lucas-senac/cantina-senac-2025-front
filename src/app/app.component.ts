5a0ac1466a3d4125b0c71392b20ea01752118a3a
Marcossou13 <marcosvinicios10000p@gmail.com>
2025-10-07 21:50:05 -0300

    Criacao do component interno card-feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { FeebacksComponent } from './components/feebacks/feebacks.component';
+import { CardFeedbackComponent} from './components/feebacks/card-feedback/card-feedback.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,FeebacksComponent],
+  imports: [RouterOutlet,LoginComponent,FeebacksComponent,CardFeedbackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/feebacks/feebacks.component.ts
+++ src/app/components/feebacks/feebacks.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardFeedbackComponent} from './card-feedback/card-feedback.component';
 
 @Component({
   selector: 'app-feebacks',
   standalone: true,
-  imports: [],
+  imports: [CardFeedbackComponent],
   templateUrl: './feebacks.component.html',
   styleUrl: './feebacks.component.css'
 })
