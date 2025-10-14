f9e73b658e78d3160a16cfce1567c277ab9b78ed
RicardoMachadoOFC <cado.machado@hotmail.com>
2025-10-14 19:55:41 -0300

    att projeto

---
+++ src/app/components/feedbacks/card-feedback/card-feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-feedback',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-feedback.component.html',
+  styleUrl: './card-feedback.component.css'
+})
+export class CardFeedbackComponent {
+
+}
