4e095089137baf10e86cebcfe9a9809e6abc72f8
Adriano <adrianocsantos@gmail.com>
2025-10-15 14:51:50 -0300

    geralzonha git add .

---
+++ src/app/components/feedbacks/card-feedback/card-feedback.component.ts
@@ -0,0 +1,17 @@
+import { Feedback } from "./feedback";
+import { Component } from "@angular/core";  
+
+@Component({
+    selector: 'app-card-feedback',
+    standalone: true,
+    imports: [],
+    templateUrl: './card-feedback.component.html',
+    styleUrls: ['./card-feedback.component.css']
+})
+export class CardFeedbackComponent {
+feedback : Feedback = {
+    nomeUsuario: "NomeDoUsuario",
+    descricao: "Descrição",
+    numComentarios: 0
+}
+};
