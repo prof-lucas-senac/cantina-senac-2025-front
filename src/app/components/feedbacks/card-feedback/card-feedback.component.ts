df85a4fd4414e7a9944e9dd4fc59fd3e3d9eb204
Marco12br <marcoantoniobueno12@gmail.com>
2025-10-14 21:42:47 -0300

    atualizacoes no card feedback(ts e feedback.ts do card).

---
+++ src/app/components/feedbacks/card-feedback/card-feedback.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Feedback } from './feedback';
+
+@Component({
+  selector: 'app-card-feedback',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-feedback.component.html',
+  styleUrl: './card-feedback.component.css'
+})
+export class CardFeedbackComponent {
+  feedback: Feedback = {
+    nomeUsuario: "NOME DO USUÁRIO",
+    descricao: "DESCRIÇÃO",
+    numComentarios: 2
+  }
+
+}
---
+++ src/app/components/feedbacks/card-feedback/feedback.ts
@@ -0,0 +1,8 @@
+
+export interface Feedback 
+{
+    nomeUsuario: string,
+    descricao: string,
+    numComentarios?: number
+
+}
