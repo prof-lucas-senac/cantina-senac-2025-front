8d09c8995bc7330f3489c5c62b8186087c72b098
Marcossou13 <marcosvinicios10000p@gmail.com>
2025-10-14 21:45:57 -0300

    Implementacao experimental  do card dinamica

--- src/app/components/feebacks/card-feedback/card-feedback.component.ts
+++ src/app/components/feebacks/card-feedback/card-feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Feedback } from './feedback';
 
 @Component({
   selector: 'app-card-feedback',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './card-feedback.component.css'
 })
 export class CardFeedbackComponent {
-
+  feedback : Feedback = {
+    nomeUsuario: "Urubu",
+    descricao : "Os urubus são aves necrófagas que se alimentam principalmente de carcaças de animais mortos",
+    numComentarios: 0,
+  }
 }
+
