165288a6abda59cf2464de8000f568bcdedf9e2b
unknown <Prog_Sis@LGE202D056043.alunos.sc.senac.br>
2025-10-21 21:49:23 -0300

    Adicionando feedbacks pra web com ajuda de decoradores

--- src/app/components/feedbacks/feedbacks.component.ts
+++ src/app/components/feedbacks/feedbacks.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CardFeedbackComponent } from './card-feedback/card-feedback.component';
+import { Feedback } from './card-feedback/feedback';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-feedbacks',
   standalone: true,
-  imports: [CardFeedbackComponent],
+  imports: [CardFeedbackComponent, NgFor],
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css'
 })
 export class FeedbacksComponent {
-
+  feedbacks : Feedback[] = [
+    {
+      descricao: "Alo garotas do mundo todo, o Rei Julian está na área e está bombando",
+      nomeUsuario: "Rei Julian",
+      numComentarios: 3
+    },
+    {
+      descricao: "Eu conheço esse cachorro, eu tenho que lembrar se não não vou dormir, PERAI",
+      nomeUsuario: "Pombo com TDAH",
+      numComentarios: 5
+    }
+  ];
 }
