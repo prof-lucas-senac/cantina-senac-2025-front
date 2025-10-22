d339396be5736cf2ba58bb07dab806a3d7d81bb5
Alexander Filho <alexanderaraldi@gmail.com>
2025-10-21 21:51:51 -0300

    implementado ngfor/diretivas e decorator/Input

--- src/app/components/feedbacks/feedbacks.component.ts
+++ src/app/components/feedbacks/feedbacks.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { CardFeedbackComponent } from "./card-feedback/card-feedback.component";
+import { NgFor, NgForOf } from "../../../../node_modules/@angular/common";
+import { Feedback } from './card-feedback/feedback';
 
 @Component({
   selector: 'app-feedbacks',
   standalone: true,
-  imports: [CardFeedbackComponent],
+  imports: [CardFeedbackComponent, NgFor],
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css'
 })
 export class FeedbacksComponent {
+feedbacks : Feedback[] = [
+ { 
+   descricao: "Salve raça",
+   nomeUsuario: "Jesus",
+   numComentarios: 1,
+ },
+ { 
+   descricao: "Salve Jorge",
+   nomeUsuario:"Noé",
+   numComentarios: 1,
+ },
+];
 }
