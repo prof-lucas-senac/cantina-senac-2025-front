875151805f2508406d8a7e33a6be080763c4bfa6
Prog_Sis <lucas.barros@prof.sc.senac.br>
2025-10-21 21:56:56 -0300

    Aula 11: Decorator e diretivas em Feedbacks.

--- src/app/components/feedbacks/feedbacks.component.ts
+++ src/app/components/feedbacks/feedbacks.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CardFeedbackComponent } from "./card-feedback/card-feedback.component";
+import { Feedback } from './card-feedback/feedback';
+import { NgFor } from '../../../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-feedbacks',
   standalone: true,
-  imports: [CardFeedbackComponent],
+  imports: [CardFeedbackComponent, NgFor],
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css'
 })
 export class FeedbacksComponent {
-
+  feedbacks : Feedback[] = [
+    {
+      descricao: "Salve família",
+      nomeUsuario: "Igor Carinha do Baú do One Piece",
+      numComentarios: 3
+    },
+    {
+      descricao: "ACORDA CARA",
+      nomeUsuario: "Monerk",
+      numComentarios: 5
+    },
+  ];
 }
