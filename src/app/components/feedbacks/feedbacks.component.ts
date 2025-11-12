4de0a69d6020e4c299ecfc701ff52a62c836488b
unknown <Prog_Sis@LGE202D056637.alunos.sc.senac.br>
2025-11-11 21:51:35 -0300

    Integração dos feedbacks do back com a interface do front

--- src/app/components/feedbacks/feedbacks.component.ts
+++ src/app/components/feedbacks/feedbacks.component.ts
@@ -2,6 +2,7 @@ import { Component,  } from '@angular/core';
 import { CardFeedbackComponent } from './card-feedback/card-feedback.component';
 import { Feedback } from './card-feedback/feedback';
 import { NgFor } from '../../../../node_modules/@angular/common';
+import { HttpClient } from '@angular/common/http'
 
 
 @Component({
@@ -12,16 +13,15 @@ import { NgFor } from '../../../../node_modules/@angular/common';
   styleUrl: './feedbacks.component.css'
 })
 export class FeedbacksComponent {
-feedbacks : Feedback[] = [
-  {
-    descricao : "Pão bão",
-    nomeUsuario : "Pedro Gabriel",
-    numComentarios : 5
-  },
-   {
-    descricao : "Pão bão",
-    nomeUsuario : "Pedro Gabriel",
-    numComentarios : 5
-   }
-];
+  
+  feedbacks : Feedback[] = [];
+
+  constructor (private http : HttpClient) {}
+
+ 
+
+  ngOnInit(): void {
+    this.http.get<Feedback[]>("http://localhost:4200/api/feedbacks")
+    .subscribe((feedbacks) => this.feedbacks = feedbacks);
+  }
 }
