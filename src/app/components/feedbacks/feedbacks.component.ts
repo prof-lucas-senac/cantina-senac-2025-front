46a66d7a69d211a10f2797d3e365d1a94aad1a24
Alexander Filho <alexanderaraldi@gmail.com>
2025-11-12 21:49:37 -0300

    implementado front e backend

--- src/app/components/feedbacks/feedbacks.component.ts
+++ src/app/components/feedbacks/feedbacks.component.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
 import { CardFeedbackComponent } from "./card-feedback/card-feedback.component";
-import { NgFor, NgForOf } from "../../../../node_modules/@angular/common";
+import { NgFor} from "../../../../node_modules/@angular/common";
 import { Feedback } from './card-feedback/feedback';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-feedbacks',
   standalone: true,
-  imports: [CardFeedbackComponent, NgFor],
+  imports: [CardFeedbackComponent, NgFor ],
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css'
 })
 export class FeedbacksComponent {
-feedbacks : Feedback[] = [
- { 
-   descricao: "Salve raça",
-   nomeUsuario: "Jesus",
-   numComentarios: 1,
- },
- { 
-   descricao: "Salve Jorge",
-   nomeUsuario:"Noé",
-   numComentarios: 1,
- },
-];
+
+feedbacks : Feedback[] = [];
+constructor(private http: HttpClient){}
+
+  ngOnInit():void{
+  this.http.get<Feedback[]>("http://localhost:4200/api/feedbacks")
+  .subscribe((feedbacks)=> this.feedbacks=feedbacks );
+}
 }
