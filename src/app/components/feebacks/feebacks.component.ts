0150cb11537f5f740c44b1aaa29c5e6067ab1ecc
Marcossou13 <Prog_Sis@LGE202D056634.alunos.sc.senac.br>
2025-11-18 21:25:38 -0300

    Correção integração com API

--- src/app/components/feebacks/feebacks.component.ts
+++ src/app/components/feebacks/feebacks.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CardFeedbackComponent} from './card-feedback/card-feedback.component';
 import { NgFor } from '@angular/common';
 import { Feedback } from './card-feedback/feedback';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-feebacks',
@@ -11,14 +12,10 @@ import { Feedback } from './card-feedback/feedback';
   styleUrl: './feebacks.component.css'
 })
 export class FeebacksComponent {
-  feedbacks : Feedback[] = [
-    {
-      nomeUsuario: "Urubu",
-      descricao: "Os urubus são aves necrófagas que se alimentam principalmente de carcaças de animais mortos"
-    },
-    {
-      nomeUsuario: "Canários",
-      descricao: "Os canários são aves conhecidas por suas cores vibrantes e cantos melodiosos. Eles têm uma origem nas Ilhas Canárias e foram domesticados a partir de aves selvagens."
+    constructor (private http : HttpClient) {}
+    feedbacks : Feedback[] = [];
+    ngOnInit() : void{
+      this.http.get<Feedback[]>("http://localhost:4200/api/feedbacks")
+      .subscribe((feedbacks) => this.feedbacks = feedbacks);
     }
-  ];
 }
